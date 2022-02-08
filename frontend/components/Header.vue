275707c609f81768300936564762f0a3ea1b7b7a
Your Name <quangtv@rabiloo.com>
2022-02-08 12:03:02 +0700

    add index layout

---
+++ frontend/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-img
+        class="align-center"
+        height="100px"
+        src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b3be1dae-3caa-4d45-be6c-3de586ba95e2/dex560o-a2730394-216b-4cd6-9792-7cf7edc3d8ce.jpg/v1/fill/w_1600,h_900,q_75,strp/pastel_sunset_by_bisbiswas_dex560o-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvYjNiZTFkYWUtM2NhYS00ZDQ1LWJlNmMtM2RlNTg2YmE5NWUyXC9kZXg1NjBvLWEyNzMwMzk0LTIxNmItNGNkNi05NzkyLTdjZjdlZGMzZDhjZS5qcGciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.UlqAza-2rpusntrvT1p_qMO4xlKmnzXmBQJhec2NyKs"
+    >
+    </v-img>
+</template>s
---
+++ frontend/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card>
+    <v-toolbar  
+      color="main"
+      class="white--text">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
